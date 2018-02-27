c4ac141b451db1751b00880998008d4b5b827b9d
PAOSTEPH\PAOLITA <pao_st55@hotmail.com>
2018-02-27 01:10:58 -0500

    18

--- files.py
+++ files.py
@@ -1,10 +1,12 @@
 import os
+#from socket import error
 
 BUFFER_SIZE = 1024
 
 def enviarFile(file, conn):
     f = open(file, 'rb')
     print 'enviando: ',file
+
     while True:
         l = f.read(BUFFER_SIZE)
         while (l):
@@ -12,11 +14,14 @@ def enviarFile(file, conn):
             print 'Tipo dato l: ', type(l)
             print 'Enviado ',repr(l)
             l = f.read(BUFFER_SIZE)
-        if not l:
-            conn.send('12345')
-            f.close()
+        #if not l:
+        #    conn.send('12345')
+        #    f.close()
             #conn.close()
-            break
+        break
+    conn.send(chr(1))
+    f.close()
+
     print 'fin enviar: ',file
 
 def recibirFile(file, conn):
@@ -24,17 +29,38 @@ def recibirFile(file, conn):
     with open(file, 'wb') as f:
         print 'file',file,' opened'
         while True:
-            data = conn.recv(BUFFER_SIZE)
-            print 'Tipo de dato de data: ',type(data)
+            try:
+                data = conn.recv(BUFFER_SIZE)
+            except:
+                print("Error de lectura.")
+                break
+            else:
+                if data:
+                    # Compatibilidad con Python 3.
+                    if isinstance(data, bytes):
+                        end = data[0] == 1
+                    else:
+                        end = data == chr(1)
+                    if not end:
+                        # Almacenar datos.
+                        f.write(data)
+                        print('data: ', (data))
+                    else:
+                        break
+            #print 'Tipo de dato de data: ',type(data)
+            """
             print('data: ', (data))
-            if '12345' in data:
+            if '1' in data:
                 f.close()
                 print 'file close()'
                 break
             # write data to a file
             f.write(data)
             data = ''
+            """
 
+    print("El archivo se ha recibido correctamente.")
+    f.close()
     print('Recibio archivo',file)
     #conn.close()
 
